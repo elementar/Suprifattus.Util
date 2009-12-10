1e41cbc4217ad69d4e69665321e6020f2119627a
Fábio D. Batista <fabio.david.batista@gmail.com>
2009-12-10 05:03:02 +0000

    Objeto "SimpleResource", para facilitar implementações de transações.
    git-svn-id: svn+ssh://elementarsistemas.com.br/data/subversion/slib-dotnet/trunk/Suprifattus.Util/src@32 06bb90e4-a37c-407d-940d-89c0ff839911

---
+++ Suprifattus.Util.Web.MonoRail/Components/Transactions/SimpleResource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Castle.Services.Transaction;
+
+namespace Suprifattus.Util.Web.MonoRail.Components.Transactions
+{
+	public class SimpleResource : IResource
+	{
+		public SimpleResource(Action commitAction, Action rollbackAction)
+		{
+			this.CommitAction = commitAction;
+			this.RollbackAction = rollbackAction;
+		}
+
+		public SimpleResource(Action commitAction)
+		{
+			this.CommitAction = commitAction;
+		}
+
+		public Action StartAction { get; set; }
+		public Action CommitAction { get; set; }
+		public Action RollbackAction { get; set; }
+
+		public void Start()
+		{
+			if (StartAction != null)
+				StartAction();
+		}
+
+		public void Commit()
+		{
+			if (CommitAction != null)
+				CommitAction();
+		}
+
+		public void Rollback()
+		{
+			if (RollbackAction != null)
+				RollbackAction();
+		}
+	}
+}
