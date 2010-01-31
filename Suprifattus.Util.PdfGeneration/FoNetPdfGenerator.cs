6f664eace2d3131f1b56e13baba4e78048467321
Fábio D. Batista <fabio.david.batista@gmail.com>
2010-01-31 01:59:32 +0000

    Novo gerador de PDF, utilizando a biblioteca FO.NET.
    git-svn-id: svn+ssh://elementarsistemas.com.br/data/subversion/slib-dotnet/trunk/Suprifattus.Util/src@34 06bb90e4-a37c-407d-940d-89c0ff839911

---
+++ Suprifattus.Util.PdfGeneration/FoNetPdfGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Xsl;
+
+using Fonet;
+
+using Suprifattus.Util.IO;
+
+namespace Suprifattus.Util.PdfGeneration
+{
+	public class FoNetPdfGenerator : IPdfGenerator
+	{
+		private readonly Regex rxPages = new Regex(@"produced (\d+) page", RegexOptions.Compiled);
+
+		public void SetConfiguration(string key, object value)
+		{
+		}
+
+		public int Generate(XmlDocument sourceDocument, Stream output)
+		{
+			var driver = FonetDriver.Make();
+			var pageCount = 0;
+			driver.OnInfo += (s, i) => UpdatePageCountHack(i, ref pageCount);
+			driver.Render(sourceDocument, output);
+
+			return pageCount;
+		}
+
+		public int Generate(XslCompiledTransform transform, XmlDocument sourceDocument, Stream output)
+		{
+			var source = new CircularStream();
+			XmlWriter w = new XmlTextWriter(source, Encoding.Default);
+
+			transform.Transform(sourceDocument, w);
+
+			return Generate(source, output);
+		}
+
+		public int Generate(Stream source, Stream output)
+		{
+			var driver = FonetDriver.Make();
+			var pageCount = 0;
+			driver.OnInfo += (s, i) => UpdatePageCountHack(i, ref pageCount);
+			driver.Render(source, output);
+
+			return pageCount;
+		}
+
+		private int UpdatePageCountHack(FonetEventArgs i, ref int pages)
+		{
+			var m = rxPages.Match(i.GetMessage());
+			if (m != null && m.Success)
+				pages += Convert.ToInt32(m.Groups[1].Value);
+			return pages;
+		}
+	}
+}
